985410e58a2cdbfdb3b3938c8b3ce3a208ab6480
Joël Séguillon <joel.seguillon@gmail.com>
2021-01-08 15:00:57 +0100

    new version of kubevirt

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Kube-virt Operator and CRDs
-export VERSION=v0.33.0
+export VERSION=v0.36.0
 kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml
 kubectl create configmap kubevirt-config -n kubevirt --from-literal debug.useEmulation=true
 kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml
