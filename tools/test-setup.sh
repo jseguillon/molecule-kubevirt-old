2f04a333c48d604d8bafd0df897809ac65ad95e8
Joël Séguillon <joel.seguillon@gmail.com>
2021-01-08 09:13:55 +0100

    not yet defeated

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -4,27 +4,11 @@ set -euxo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
-# TODO : check if really needed
-sudo apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager libvirt-daemon-system
-sudo systemctl restart libvirtd
-
-sleep 30 && journalctl  -u libvirtd | cat 
-
-# This script is for Kind 
-curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.9.0/kind-$(uname)-amd64" 
-chmod +x ./kind 
-./kind create cluster
-
-sleep 30 
-
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.20.0/bin/linux/amd64/kubectl; chmod +x ./kubectl
-
 # Kube-virt Operator and CRDs
 export VERSION=v0.33.0
-./kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml
-./kubectl create configmap kubevirt-config -n kubevirt --from-literal debug.useEmulation=true
-./kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml
+kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml
+kubectl create configmap kubevirt-config -n kubevirt --from-literal debug.useEmulation=true
+kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml
 
 # virtcl tool 
 export ARCH=linux-amd64
@@ -34,9 +18,9 @@ sudo install virtctl /usr/local/bin
 
 # CDI 
 export VERSION=$(curl -s https://github.com/kubevirt/containerized-data-importer/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
-./kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
-./kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-cr.yaml
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-cr.yaml
 
 sleep 30 
 
-./kubectl create -f ${DIR}/test-rolebinding.yaml
+kubectl create -f ${DIR}/test-rolebinding.yaml
