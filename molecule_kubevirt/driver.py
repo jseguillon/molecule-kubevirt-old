521c2605ff612d3857de695b5708d0724421c79d
Joël Séguillon <joel.seguillon@gmail.com>
2021-01-18 21:28:53 +0100

    rework log a bit

--- molecule_kubevirt/driver.py
+++ molecule_kubevirt/driver.py
@@ -30,21 +30,16 @@ log = logger.get_logger(__name__)
 
 class KubeVirt(Driver):
     """
-    Kubevirt Driver Class. # FIXME doc
+    Kubevirt Driver Class.
 
     The class responsible for managing `Docker`_ containers.  `Docker`_ is
     the default driver used in Molecule.
 
-    Molecule leverages Ansible's `docker_container`_ module, by mapping
+    Molecule leverages Ansible's `kubevirt_vm`_ module, by mapping
     variables from ``molecule.yml`` into ``create.yml`` and ``destroy.yml``.
 
-    Molecule leverages Ansible's `docker_network`_ module, by mapping variable
-    ``docker_networks`` into ``create.yml`` and ``destroy.yml``.
-
-    .. _`docker_container`: https://docs.ansible.com/ansible/latest/modules/docker_container_module.html
-    .. _`docker_network`: https://docs.ansible.com/ansible/latest/modules/docker_network_module.html
-    .. _`Docker Security Configuration`: https://docs.docker.com/engine/reference/run/#security-configuration
-    .. _`Docker daemon socket options`: https://docs.docker.com/engine/reference/commandline/dockerd/#daemon-socket-option
+    .. _`kubevirt_vm`: https://docs.ansible.com/ansible/latest/collections/community/general/kubevirt_vm_module.html
+    .. _`k8s`: https://docs.ansible.com/ansible/latest/collections/community/kubernetes/k8s_module.html
 
     .. code-block:: yaml
 
@@ -52,117 +47,38 @@ class KubeVirt(Driver):
           name: kubevirt
         platforms:
           - name: instance
-            hostname: instance
+            namespace: default
+            wait_timeout: 300
+            memory: 2Gi
+            cpu_cores: (omit)
+            machine_type: q35
+            cpu_model: (omit)
+            headless: (omit)
+            hugepage_size: (omit)
+            cpu_limit: (omit)
+            cpu_shares: (omit)
+            ephemeral: (omit)
             image: image_name:tag
-            dockerfile: Dockerfile.j2
-            pull: True|False
-            pre_build_image: True|False
-            registry:
-              url: registry.example.com
-              credentials:
-                username: $USERNAME
-                password: $PASSWORD
-                email: user@example.com
-                user: root
-            override_command: True|False
-            command: sleep infinity
-            tty: True|False
-            pid_mode: host
-            privileged: True|False
-            security_opts:
-              - seccomp=unconfined
-            devices:
-              - /dev/fuse:/dev/fuse:rwm
-            volumes:
-              - /sys/fs/cgroup:/sys/fs/cgroup:ro
-            keep_volumes: True|False
-            tmpfs:
-              - /tmp
-              - /run
-            capabilities:
-              - SYS_ADMIN
-            sysctls:
-              net.core.somaxconn: 1024
-              net.ipv4.tcp_syncookies: 0
-            exposed_ports:
-              - 53/udp
-              - 53/tcp
-            published_ports:
-              - 0.0.0.0:8053:53/udp
-              - 0.0.0.0:8053:53/tcp
-            ulimits:
-              - nofile:262144:262144
-            dns_servers:
-              - 8.8.8.8
-            etc_hosts: "{'host1.example.com': '10.3.1.5'}"
-            kubevirt_networks:
-              - name: foo
-                ipam_config:
-                  - subnet: '10.3.1.0/24'
-                    gateway: '10.3.1.254'
-            networks:
-              - name: foo
-              - name: bar
-            network_mode: host
-            purge_networks: true
-            kubevirt_host: tcp://localhost:12376
-            cacert_path: /foo/bar/ca.pem
-            cert_path: /foo/bar/cert.pem
-            key_path: /foo/bar/key.pem
-            tls_verify: true
-            env:
-              FOO: bar
-            restart_policy: on-failure
-            restart_retries: 1
-            buildargs:
-                http_proxy: http://proxy.example.com:8080/
 
-    If specifying the `CMD`_ directive in your ``Dockerfile.j2`` or consuming a
-    built image which declares a ``CMD`` directive, then you must set
-    ``override_command: False``. Otherwise, Molecule takes care to honour the
-    value of the ``command`` key or uses the default of ``bash -c "while true;
-    do sleep 10000; done"`` to run the container until it is provisioned.
+    Image MUST be accessible on Kubernetes workers running Kubevirt. This driver
+    provides no service for building images. Solutions using CDI may be found in
+    later version .
 
-    When attempting to utilize a container image with `systemd`_ as your init
-    system inside the container to simulate a real machine, make sure to set
-    the ``privileged``, ``volumes``, ``command``, and ``env``
-    values. An example using the ``centos:8`` image is below:
+    Minimal authorizations are required for the molecule runner via ServiceAccount:
+    .. code-block::yaml
+        roleRef:
+          apiGroup: rbac.authorization.k8s.io
+          kind: ClusterRole
+          name: kubevirt.io:edit
 
-    .. note:: Do note that running containers in privileged mode is considerably
-              less secure. For details, please reference `Docker Security
-              Configuration`_
+    .. note:: This driver does not require specific container privileges.
 
-    .. note:: With the environment variable ``DOCKER_HOST`` the user can bind
-              Molecule to a different `Docker`_ socket than the default
-              ``unix:///var/run/docker.sock``. ``tcp``, ``fd`` and ``ssh``
-              socket types can be configured. For details, please reference
-              `Docker daemon socket options`_.
-
-    .. code-block:: yaml
-        FIXME : change doc
-        platforms:
-        - name: instance
-          image: centos:8
-          privileged: true
-          volumes:
-            - "/sys/fs/cgroup:/sys/fs/cgroup:rw"
-          command: "/usr/sbin/init"
-          tty: True
-          env:
-            container: docker
+    .. note:: Default ssh access is curently set via a ClusterIp Service. This
+        may change in futur releases.
 
     .. code-block:: bash
 
         $ python3 -m pip install molecule[kubevirt]
-
-    When pulling from a private registry, it is the user's discretion to decide
-    whether to use hard-code strings or environment variables for passing
-    credentials to molecule.
-
-    .. important::
-
-        Hard-coded credentials in ``molecule.yml`` should be avoided, instead use
-        `variable substitution`_.
 
     Provide a list of files Molecule will preserve, relative to the scenario
     ephemeral directory, after any ``destroy`` subcommand execution.
@@ -174,9 +90,7 @@ class KubeVirt(Driver):
           safe_files:
             - foo
 
-    .. _`Docker`: https://www.docker.com
-    .. _`systemd`: https://www.freedesktop.org/wiki/Software/systemd/
-    .. _`CMD`: https://docs.docker.com/engine/reference/builder/#cmd
+    .. _`Kubevirt`: https://kubevirt.io/
     """  # noqa
 
     def __init__(self, config=None):
@@ -198,7 +112,7 @@ class KubeVirt(Driver):
     def default_safe_files(self):
         return (
             []
-        )  # FIXME : should ssh-keys generated by create.yml playbook be set as so called safe_file ?
+        )  # FIXME : should ssh-keys generated by create.yml playbook be set as safe_file ?
 
     @property
     def login_cmd_template(self):
--- tools/gather_logs.sh
+++ tools/gather_logs.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 # Kail sends any logs from default namespace
-kail -n default 2>&1 > /tmp/kail.log &
-# Tail the kail log file for output in github actions
-tail -f /tmp/kail.log &
+sudo kail -n default 2>&1 | tee /tmp/kail.log &
 
 # Backgroung wait for Pod reflecting VM to be reday then log console
 until kubectl wait --timeout=15m --for=condition=Ready pod -l kubevirt.io=virt-launcher --namespace default;
-
-  do echo "Still Waiting Pod to start..."; sleep 5; done
-
-  echo "Starting virtctl console" >> /tmp/virtcl.txt
-  sudo script -e -c "virtctl console instance" >> /tmp/virtcl.txt
-  sudo tail -f /tmp/virtcl.txt &
-
+  do echo "Still Waiting Pod to start..."; sleep 5;
 done
+
+echo "Starting virtctl console" >> /tmp/virtcl.txt
+sudo script -e -c "virtctl console instance" >> /tmp/virtcl.txt
+sudo tail -f /tmp/virtcl.txt &
