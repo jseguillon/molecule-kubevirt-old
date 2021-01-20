afa2c68e81cd19337b6f69463b6382037ee52038
Joël Séguillon <joel.seguillon@gmail.com>
2021-01-20 23:20:03 +0100

    hidden github restrict

--- tools/log_console.sh
+++ tools/log_console.sh
@@ -5,7 +5,7 @@ while true; do
     do echo "Still Waiting Pod to start..."; sleep 5;
   done
 
-  LOGFILE="virtcl-console-$(date '+%Y-%m-%d+%H:%M:%S').log"
+  LOGFILE="virtcl-console-$(date '+%Y-%m-%d-%H:%M:%S').log"
   echo "Starting virtctl console" >> /tmp/${LOGFILE}
   sudo script -e -c "virtctl console instance" >> /tmp/${LOGFILE}
 done
